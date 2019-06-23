410f910507621f556d34948e4b85e7776806bb21
ASHOK MAHARJAN <ashok997@gmail.com>
2019-06-23 15:19:45 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -34,6 +34,8 @@ class MusicLibraryController
           self.list_genres
         when 'list artist'
           self.list_songs_by_artist
+        when 'list genre'
+          self.list_songs_by_genre
         
       end
       
